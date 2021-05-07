25d16738daee906e70db9b12eb25d807b901b8f1
Chris <chrischillogallid64@outlook.com>
2021-05-07 13:55:41 -0500

    feat: add exercises list

--- src/Ejercicios/Ejercicio_11.java
+++ src/Ejercicios/Ejercicio_11.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio_11 {
     public static void main(String[] args) {
-        /*Usando el métod lenght*/
+        /*Usando el método lenght*/
 
         String cadena;
         Scanner entrada = new Scanner(System.in);
